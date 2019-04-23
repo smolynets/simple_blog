9432ad8deba7f85396f73159468d09c19df17f5c
Oleh Smolynets <smolynets@gmail.com>
2019-04-23 22:13:39 +0300

    Add ALLOWED_HOSTS to sample_local_settings.py

--- simple_blog/sample_local_settings.py
+++ simple_blog/sample_local_settings.py
@@ -3,6 +3,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ALLOWED_HOSTS = ["*"]
+
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
